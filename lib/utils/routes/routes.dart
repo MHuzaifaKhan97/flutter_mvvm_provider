b305ee7a8d5a3c8abf8627207e3255e5b15efb74
Muhammad Huzaifa Khan <47402181+MHuzaifaKhan97@users.noreply.github.com>
2022-09-01 23:18:07 +0500

    Add Routes for Signup

--- lib/utils/routes/routes.dart
+++ lib/utils/routes/routes.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_mvvm_provider/utils/routes/routes_name.dart';
 import 'package:flutter_mvvm_provider/view/home_screen.dart';
 import 'package:flutter_mvvm_provider/view/login_view.dart';
+import 'package:flutter_mvvm_provider/view/signup_screen.dart';
 
 class Routes {
   static Route<dynamic> generateRoute(RouteSettings settings) {
@@ -10,6 +11,8 @@ class Routes {
         return MaterialPageRoute(builder: (context) => const LoginView());
       case RoutesName.home:
         return MaterialPageRoute(builder: (context) => const HomeScreen());
+      case RoutesName.signup:
+        return MaterialPageRoute(builder: (context) => const SignUpView());
       default:
         return MaterialPageRoute(
             builder: (context) => const Scaffold(
